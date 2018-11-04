13341032a4f18bd0d8a61cc757666da0708bb2b1
Tony Gam <33107394+agamble33@users.noreply.github.com>
2018-11-03 22:11:01 -0500

    update

--- js/index.js
+++ js/index.js
@@ -126,3 +126,24 @@ function myFunction() {
     navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", "");
   }
 }
+
+// Email obfuscator script 2.1 by Tim Williams, University of Arizona
+// Random encryption key feature coded by Andrew Moulden
+// This code is freeware provided these four comment lines remain intact
+// A wizard to generate this code is at http://www.jottings.com/obfuscator/
+{ coded = "KsK5Qhnzr@s5KWh.d95"
+  key = "0NlIxmnWFtVc2JGaPg17BhvE5DMbTodzyYKSskX3pfHrLAQu9eiU86jOqCZ4Rw"
+  shift=coded.length
+  link=""
+  for (i=0; i<coded.length; i++) {
+    if (key.indexOf(coded.charAt(i))==-1) {
+      ltr = coded.charAt(i)
+      link += (ltr)
+    }
+    else {     
+      ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
+      link += (key.charAt(ltr))
+    }
+  }
+document.write("<a href='mailto:"+link+"'>"+link+"</a>")
+}
